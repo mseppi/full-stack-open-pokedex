2c671f5ce038fac959da5db5f32b39524639539e
mseppi <miikka.seppi123@gmail.com>
2025-10-09 05:01:44 +0300

    add one e2e test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,10 @@ test.describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('navigating to a pokemon page works', async ({ page }) => {
+    await page.goto('http://localhost:5000')
+    await page.getByRole('link', { name: /ivysaur/i }).click()
+    await page.waitForURL('**/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
